56235e218da2e34742aa2603efcd8be45f06134d
志鸿思荣 <1107438082@qq.com>
2018-10-14 16:49:44 +0800

    登陆页面
    1. 登陆页面
    2.添加token

--- src/main.js
+++ src/main.js
@@ -20,39 +20,52 @@ new Vue({
     router
 }).$mount('#app');
 
-function axiosF(url,type,data,callBackRes,callBackErr) {
-    let param={},paramArr=[],urlP='';
-    for( let key in data){
-        paramArr.push(key+"="+ data[key])
+function axiosF(url, type, data, callBackRes, callBackErr) {
+  let token = window.sessionStorage.getItem('token'), headers = {};
+  if (token) {
+    headers = {'token': token};
+  }
+  let param = {}, paramArr = [], urlP = '';
+  for (let key in data) {
+    paramArr.push(key + '=' + data[key]);
+  }
+  switch (type) {
+    case 'get' :
+      urlP = paramArr.length ? baseURL + url + '?' + paramArr.join('&') : baseURL + url;
+      param = {
+        method: 'get',
+        headers: headers,
+        url: urlP
+      };
+      break;
+    case 'post':
+      urlP = baseURL + url;
+      param = {
+        method: 'post',
+        headers: headers,
+        url: urlP,
+        data: data
+      };
+      break;
+    default:
+      urlP = baseURL + url;
+      param = {
+        method: type,
+        url: urlP
+      };
+      break;
+  }
+  axios(param).then(res => {
+    console.log(res);
+    let newToken = res.headers.newToken;
+    token = res.headers.token;
+    token = newToken || token || '';
+    if (token) {
+      window.sessionStorage.setItem('token', token);
     }
-    switch (type){
-        case 'get' :
-            urlP=paramArr.length?baseURL+url+"?"+paramArr.join('&'):baseURL+url;
-            param={
-                method:'get',
-                url:urlP
-            };
-            break;
-        case 'post':
-            urlP=baseURL+url;
-            param={
-                method:'post',
-                url:urlP,
-                data: data
-            };
-            break;
-        default:
-            urlP=baseURL+url;
-            param={
-                method:type,
-                url:urlP,
-            };
-            break;
-    }
-    axios(param).then(res=>{
-       return callBackRes(res)
-    }).catch(err=>{
-        debugger
-         return callBackErr(err)
-    });
+    console.log(token);
+    return callBackRes(res);
+  }).catch(err => {
+    return callBackErr(err);
+  });
 }
--- src/papes/login/login.vue
+++ src/papes/login/login.vue
@@ -2,10 +2,19 @@
     <div id="login_page">
         <el-form class="login_form" :label-position="labelPosition" label-width="80px" :model="formLabelAlign">
             <el-form-item label="用户名">
-                <el-input v-model="formLabelAlign.name"></el-input>
+                <el-input v-model="formLabelAlign.loginName"></el-input>
             </el-form-item>
             <el-form-item label="密码">
-                <el-input v-model="formLabelAlign.region" ></el-input>
+                <el-input v-model="formLabelAlign.password" ></el-input>
+            </el-form-item>
+            <el-form-item label="用户角色">
+              <el-select v-model="formLabelAlign.userType" >
+                <el-option value="0" key="0" label="管理员"></el-option>
+                <el-option value="1" key="1" label="班主任"></el-option>
+                <el-option value="2" key="2" label="教师"></el-option>
+                <el-option value="3" key="3" label="学生"></el-option>
+                <el-option value="4" key="4" label="家长"></el-option>
+              </el-select>
             </el-form-item>
             <el-form-item>
                 <el-button @click="onLoginOut">登陆</el-button>
@@ -22,14 +31,15 @@
             return {
                 labelPosition: 'right',
                 formLabelAlign: {
-                    name: '',
-                    region: ''
+                    loginName: '',
+                    password: '',
+                    userType: '1'
                 }
             };
         },
         methods:{
             onLoginOut(){
-                this.$axiosF('','post',{},res=>{
+                this.$axiosF('user/login','post',this.formLabelAlign,res=>{
                     if(res.data.success){
                         this.$message({
                             showClose: true,
